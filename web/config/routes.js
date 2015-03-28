f7ac796dbc352dbb0ddc41f33002097ab8bf2943
Yuan Xulei <xyuan@mozilla.com>
2015-03-28 15:37:39 +0800

    Add routes and policies for authentication

--- web/config/routes.js
+++ web/config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': {
     view: 'homepage'
-  }
+  },
 
   /***************************************************************************
   *                                                                          *
@@ -46,4 +46,15 @@ module.exports.routes = {
   *                                                                          *
   ***************************************************************************/
 
+  // Authentication related routes from sails-generate-auth
+  'get /login': 'AuthController.login',
+  'get /logout': 'AuthController.logout',
+  'get /register': 'AuthController.register',
+
+  'post /auth/local': 'AuthController.callback',
+  'post /auth/local/:action': 'AuthController.callback',
+
+  'get /auth/:provider': 'AuthController.provider',
+  'get /auth/:provider/callback': 'AuthController.callback',
+  'get /auth/:provider/:action': 'AuthController.callback'
 };

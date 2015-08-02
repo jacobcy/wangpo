9dc53cb78a8194339227a90040a29203a7d4daa5
Yuan Xulei <hi@yxl.name>
2015-08-02 18:19:15 +0800

    微博消息图片转存七牛服务器

--- web/api/controllers/MainController.js
+++ web/api/controllers/MainController.js
@@ -4,35 +4,6 @@
  * @description :: Server-side logic for managing Maincontrollers
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-var qiniu = require('qiniu');
-var md5 = require('md5');
-var request = require('request');
-
-qiniu.conf.ACCESS_KEY = sails.config.qiniu.AccessKey;
-qiniu.conf.SECRET_KEY = sails.config.qiniu.SecretKey;
-
-const QINIU_BUCKET = sails.config.qiniu.bucket;
-const IMAGE_SERVER_URL = 'http://' + QINIU_BUCKET + '.qiniudn.com/';
-const UPLOAD_LIMIT_IN_MBYTES = sails.config.qiniu.uploadLimitInMbytes;
-
-function upload_qiniu_file(name, content, type, cb) {
-  var putPolicy = new qiniu.rs.PutPolicy(sails.config.qiniu.bucket);
-  var uptoken = putPolicy.token();
-  var extra = new qiniu.io.PutExtra();
-  extra.mimeType = type;
-  qiniu.io.put(uptoken, name, content, extra, function(err, ret) {
-    if(!err) {
-      // 上传成功
-      cb(null, IMAGE_SERVER_URL + ret.key);
-    } else {
-      // 上传失败, 处理返回代码
-      console.log('Failed to upload qiniu file: ' + err);
-      cb(err, null);
-      // http://developer.qiniu.com/docs/v6/api/reference/codes.html
-    }
-  });
-}
-
 module.exports = {
   index: function(req, res) {
     return res.view('index', {
@@ -59,42 +30,8 @@ module.exports = {
         error: 'URL不能为空'
       });
     }
-    request({
-      url: url,
-      encoding: null
-    }, function (err, r, body) {
-      if (err) {
-        console.error('[saveImage] Failed to get image ' + url + ' with error: ' + err);
-        res.json({
-          error: err
-        });
-        return;
-      }
-      type = r.headers['content-type'];
-      if (type.indexOf('image') === -1) {
-        // 传入的不是图片
-        return res.json({
-          error: '输入的文件不是图片，格式为: ' + type
-        });
-      }
-      // 图片大小限制(单位M)
-      if (body.length > UPLOAD_LIMIT_IN_MBYTES * 1024 * 1024) {
-        return res.json({
-          error: '图片应小于' + UPLOAD_LIMIT_IN_MBYTES + 'M'
-        });
-      }
-      var name = md5(url + Date.now());
-      upload_qiniu_file(name, body, type, function(err, url) {
-        if (err) {
-          res.json({
-            error: err
-          });
-          return;
-        }
-        res.json({
-          url: url
-        });
-      });
+    sails.services.utils.saveQiniuImageFromUrl(url, function(json) {
+      res.json(json);
     });
   }
 };
--- web/api/services/weibofans/index.js
+++ web/api/services/weibofans/index.js
@@ -101,12 +101,18 @@ function handleTextMessage(sender_id, text, cb) {
 function handleImageMessage(sender_id, tovfid, cb) {
   var imageUrl = 'https://upload.api.weibo.com/2/mss/msget?access_token=' + sails.config.weibo.access_token +
                 '&fid=' + tovfid;
-  ChatManager.process(sender_id, null, imageUrl, function(msg) {
-    if (!msg) {
-      // 默认情况显示帮助信息
-      msg = ReplyMessages.help;
+  sails.services.utils.saveQiniuImageFromUrl(imageUrl, function(json) {
+    if (json.error) {
+      cb('图片保存失败:' + json.error);
+      return;
     }
-    cb(msg);
+    ChatManager.process(sender_id, null, json.url, function(msg) {
+      if (!msg) {
+        // 默认情况显示帮助信息
+        msg = ReplyMessages.help;
+      }
+      cb(msg);
+    });
   });
 }
 

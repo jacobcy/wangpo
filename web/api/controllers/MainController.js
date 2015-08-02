dd4441135ad9faac4642e4660436dff60c33ab3f
Yuan Xulei <hi@yxl.name>
2015-08-02 17:47:40 +0800

    添加注释说明转存图片到七牛服务器的接口。

--- web/api/controllers/MainController.js
+++ web/api/controllers/MainController.js
@@ -47,7 +47,10 @@ module.exports = {
   },
 
   /**
-   * /main/saveImage?url=
+   * 转存网络图片到七牛服务器接口
+   * /main/saveImage?url=[URL]
+   * @param url 待存储图片的地址
+   * @return 成功返回 {url: 七牛服务器图片URL}, 失败返回 {error: 错误信息}
    */
   saveImage: function(req, res, body) {
     var url = req.param('url') || '';

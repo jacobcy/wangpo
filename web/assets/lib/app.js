03e92b1036bf71339a328c74eab3c0f9955297f6
Yuan Xulei <hi@yxl.name>
2015-11-01 00:50:31 +0800

    修正首页图片

--- web/assets/lib/app.js
+++ web/assets/lib/app.js
@@ -4,60 +4,54 @@ app.controller('appCtrl',['$scope','fgDelegate',function($scope,fgDelegate){
   $scope.items = [
     {
       id:1,
-      img:'http://placehold.it/300x600/E97452/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/84e28fed15a61ed45c53be99802958b3',
+      name:'温暖中的小迪,女,27,175,北京'
     },
     {
       id:2,
-      img:'http://placehold.it/300x300/4C6EB4/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/a67b80c4dafd5b1adc213134a5c80379',
+      name:'温暖中的小迪,女,27,175,北京'
     },
     {
       id:3,
-      img:'http://placehold.it/300x250/449F93/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/a45f00537ded203d9c8437b48dcbfc46',
+      name:'温暖中的小迪,女,27,175,北京'
     },
     {
       id:4,
-      img:'http://placehold.it/200x320/936FBC/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/87ee4835d5563c158a54dec8949036f4',
+      name:'温暖中的小迪,女,27,175,北京'
     },
     {
       id:5,
-      img:'http://placehold.it/400x500/D25064/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/f00efcb4932447985fb426eff905ba7d',
+      name:'碎玉005,男,25,168,北京'
     },
     {
       id:6,
-      img:'http://placehold.it/300x200/CF364A/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/4db17fe5e82ff5d78c37a2fed198cda8',
+      name:'做一枚安静的美男子,男,21,172,北京'
     },
     {
       id:7,
-      img:'http://placehold.it/300x400/E59649/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/5c1960a8c3aa7706ccf22418f20e3e1b',
+      name:'做一枚安静的美男子,男,21,172,北京'
     },
     {
       id:8,
-      img:'http://placehold.it/350x500/75A0CC/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/1d987da40c3f3bcee19bf0724e38d2fe',
+      name:'做一枚安静的美男子,男,21,172,北京'
     },
     {
       id:9,
-      img:'http://placehold.it/300x200/4296AD/fff',
-      name:'Lorem ipsum dolor sit amet'
+      img:'http://vq-script.qiniudn.com/a8125c6c89767dd0a8d9c215694fa10f',
+      name:'做一枚安静的美男子,男,21,172,北京'
     },
     {
       id:10,
-      img:'http://placehold.it/300x400/9FDBC7/fff',
-      name:'Lorem ipsum dolor sit amet'
-    },
-    {
-      id:11,
-      img:'http://placehold.it/300x300/4E8EF7/fff',
-      name:'Lorem ipsum dolor sit amet'
-    },
-
+      img:'http://vq-script.qiniudn.com/fed5c73bea5ade46585d940ac7774a79',
+      name:'轩辕镜妍,女,28,172,北京'
+    }
   ]
 
   $scope.addItem = function(){
